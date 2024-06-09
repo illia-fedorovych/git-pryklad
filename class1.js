b61f2eb6a5b35efa836c796dd68257bf977c41da
illia-fedorovych <i.fedorovych@nodeart.io>
2024-06-09 20:33:43 +0300

    illustration

--- class1.js
+++ class1.js
@@ -3,6 +3,19 @@ class Class1 {
         this.name = 'Class1';
     }
     getName() {
-        return this.name + ' from Class1';
+        return this.name;
     }
 }
+
+--- x------x----- x-- - x-- - x----- x(class2)------------x(master, merge commit)
+\                     /
+\------x(class1)----- /
+
+--- x------x----- x-- - x-- - x----- x(class2)---------
+\
+\------X(class1, master)
+
+
+git remote add origin https://github.com/illia-fedorovych/git-pryklad.git
+git branch - M main
+git push - u origin main
